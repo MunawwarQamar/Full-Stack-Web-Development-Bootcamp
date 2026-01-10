96e4ad28fae9cd898abed70a5b0dcea4fc169c26
MunawwarQamar <Munawwarbaba2004@gmail.com>
2026-01-10 10:34:58 +0200

    Add Js  task 04 - Video Preview

--- Web/JS/4-Video Preview/script.js
+++ Web/JS/4-Video Preview/script.js
@@ -1,1 +1,12 @@
 console.log("page loaded...");
+
+var video = document.querySelector("video");
+
+video.addEventListener("mouseover", () => {
+  video.play();
+});
+
+video.addEventListener("mouseout", () => {
+  video.pause();
+  video.currentTime = 0;
+});
